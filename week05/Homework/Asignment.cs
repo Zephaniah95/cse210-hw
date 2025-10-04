26a64df1b4e77856f487f89da5c463bc66a9f4a9
unknown <Inmerizephaniah@byupathway.edu>
2025-10-04 08:32:11 +0100

    Update

---
+++ week05/Homework/Asignment.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Option 1: Make student name accessible for derived classes
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test base Assignment
+        Assignment a1 = new Assignment("Imegi Nmeri Zephaniah", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        // Test MathAssignment
+        MathAssignment m1 = new MathAssignment("Fredshed Catherine", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment w1 = new WritingAssignment("Christopher Fredshed", "Zoology", "The Role of Pollinators in Biodiversity Conservation");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
